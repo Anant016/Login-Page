cde117ecc582089f5ab22e476b49bdc3a68b75bc
anant <anantrungta1999@gmail.com>
2018-10-09 23:55:11 +0530

    pass change added

--- config/database.js
+++ config/database.js
@@ -2,7 +2,14 @@
 
 module.exports={
 
-    database:'yourdblink',
+    database:'mongodb://anu:cool123@ds045087.mlab.com:45087/anu',
 
     secret:'mysecret'
 }
+
+
+
+//mongodb://anu:cool123@ds045087.mlab.com:45087/anu
+
+//mongodb://localhost:27017/anu
+
