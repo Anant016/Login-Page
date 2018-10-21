c8dfc3b102183cb7bcdf316cf84434daf0f21005
Anant Rungta <anantrungta1999@gmail.com>
2018-10-21 20:37:26 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,14 +2,11 @@
 
 module.exports={
 
-    database:'mongodb://anu:cool123@ds045087.mlab.com:45087/anu',
+    database:'your db',
 
-    secret:'mysecret'
+    secret:'your secrett'
 }
 
 
 
-//mongodb://anu:cool123@ds045087.mlab.com:45087/anu
-
-//mongodb://localhost:27017/anu
 
